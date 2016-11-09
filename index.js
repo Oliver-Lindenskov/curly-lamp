ae0ed4c379671d367f0f22f58a93d7f4c18b0df8
Oliver lindenskov <Oliver.lindenskov157@gmail.com>
2016-11-09 15:38:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ io.on('connection', function(socket){
         console.log(users);
     });
 });
-http.listen(8080, function(){
-   console.log('listening on *:3000');
-});
-
-// http.listen(process.env.PORT || 3000, function(){
-//   console.log("Express server listening on port"  + this.address().port);
+// http.listen(8080, function(){
+//   console.log('listening on *:3000');
 // });
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port"  + this.address().port);
+});
